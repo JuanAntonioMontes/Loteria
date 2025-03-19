833989dee32807691c9c2a1edc0f05c2520531ed
JuanGCarballo <juan.gcarballo@babelgroup.com>
2025-03-19 12:04:30 +0100

    feat: add jugador field to Apuesta and implement duplicate check in createApuesta method

--- src/main/java/com/ejercicio/loteria/Services/ApuestaService.java
+++ src/main/java/com/ejercicio/loteria/Services/ApuestaService.java
@@ -8,6 +8,9 @@ import com.ejercicio.loteria.entities.Jugador;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.net.http.HttpResponse;
+import java.util.List;
+
 @Service
 public class ApuestaService {
 
@@ -21,9 +24,22 @@ public class ApuestaService {
     }
 
     @Transactional
-    public Apuesta createApuesta(ApuestaDTO apuestadto, Integer juagadorId) {
-        Jugador jugador = jugadorRepository.findById(juagadorId).orElseThrow();
+    public Apuesta createApuesta(ApuestaDTO apuestadto, Integer jugadorId) {
+
+        List<Apuesta> apuestaLista = apuestaRepository.findAll();
+        Jugador jugador = jugadorRepository.findById(jugadorId).orElseThrow();
         Apuesta apuesta = new Apuesta(apuestadto.numero1(), apuestadto.numero2(), apuestadto.numero3(), apuestadto.numero4(), apuestadto.numero5(), apuestadto.numero6(), jugador);
+        for (Apuesta a:apuestaLista){
+            if (a.getJugador().getId().equals(jugadorId) &&
+                    a.getNumero1().equals(apuesta.getNumero1()) &&
+                    a.getNumero2().equals(apuesta.getNumero2()) &&
+                    a.getNumero3().equals(apuesta.getNumero3()) &&
+                    a.getNumero4().equals(apuesta.getNumero4()) &&
+                    a.getNumero5().equals(apuesta.getNumero5()) &&
+                    a.getNumero6().equals(apuesta.getNumero6())) {
+                throw new RuntimeException("Ya existe una apuesta con esos números");
+            }
+        }
         return apuestaRepository.save(apuesta);
     }
 }

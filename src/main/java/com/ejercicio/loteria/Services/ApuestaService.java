cfcdd1a22f9189d5be1a2dc595ad14ab336770eb
Juan Antonio Montes <juanantonio.montes@babelgroup.com>
2025-03-19 12:25:18 +0100

    random

--- src/main/java/com/ejercicio/loteria/Services/ApuestaService.java
+++ src/main/java/com/ejercicio/loteria/Services/ApuestaService.java
@@ -19,6 +19,8 @@ public class ApuestaService {
 
     private final JugadorRepository jugadorRepository;
 
+    Random random = new Random();
+
     public ApuestaService(ApuestaRepository apuestaRepository, JugadorRepository jugadorRepository) {
         this.apuestaRepository = apuestaRepository;
         this.jugadorRepository = jugadorRepository;
@@ -31,6 +33,31 @@ public class ApuestaService {
         List<Apuesta> apuestaLista = apuestaRepository.findAll();
         Jugador jugador = jugadorRepository.findById(jugadorId).orElseThrow();
         Apuesta apuesta = new Apuesta(apuestadto.numero1(), apuestadto.numero2(), apuestadto.numero3(), apuestadto.numero4(), apuestadto.numero5(), apuestadto.numero6(), jugador);
+        for (Apuesta a:apuestaLista){
+            if (a.getJugador().getId().equals(jugadorId) &&
+                    a.getNumero1().equals(apuesta.getNumero1()) &&
+                    a.getNumero2().equals(apuesta.getNumero2()) &&
+                    a.getNumero3().equals(apuesta.getNumero3()) &&
+                    a.getNumero4().equals(apuesta.getNumero4()) &&
+                    a.getNumero5().equals(apuesta.getNumero5()) &&
+                    a.getNumero6().equals(apuesta.getNumero6())) {
+                throw new RuntimeException("Ya existe una apuesta con esos números");
+            }
+        }
+        return apuestaRepository.save(apuesta);
+    }
+
+    public Apuesta createApuestaRandom(ApuestaDTO apuestadto, Integer jugadorId) {
+        validarNumerosApuesta(apuestadto);
+        List<Apuesta> apuestaLista = apuestaRepository.findAll();
+        Jugador jugador = jugadorRepository.findById(jugadorId).orElseThrow();
+        Apuesta apuesta = new Apuesta(apuestadto.numero1(),
+                apuestadto.random.nexInt(49)+1,
+                apuestadto.random.nexInt(49)+1,
+                apuestadto.random.nexInt(49)+1,
+                apuestadto.random.nexInt(49)+1,
+                apuestadto.random.nexInt(49)+1,
+                jugador);
         for (Apuesta a:apuestaLista){
             if (a.getJugador().getId().equals(jugadorId) &&
                     a.getNumero1().equals(apuesta.getNumero1()) &&

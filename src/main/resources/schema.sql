a4241efedecabec73c060530cfc9ade9634df4b1
Jaime Ramos Lara <jaime.ramos@babelgroup.com>
2025-03-19 11:15:54 +0100

    solucionado error

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS `jugador` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `nombre` NUMERIC(2) NOT NULL,);
+    `nombre` VARCHAR(50) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS `apuesta` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `jugador` INT NOT NULL,
-    `numero1` NUMERIC(2) NOT NULL,
-    `numero2` NUMERIC(2) NOT NULL,
-    `numero3` NUMERIC(2) NOT NULL,
-    `numero4` NUMERIC(2) NOT NULL,
-    `numero5` NUMERIC(2) NOT NULL,
-    `numero6` NUMERIC(2) NOT NULL,
+    `numero1` TINYINT NOT NULL,
+    `numero2` TINYINT NOT NULL,
+    `numero3` TINYINT NOT NULL,
+    `numero4` TINYINT NOT NULL,
+    `numero5` TINYINT NOT NULL,
+    `numero6` TINYINT NOT NULL,
     FOREIGN KEY (jugador) REFERENCES jugador(id));
 

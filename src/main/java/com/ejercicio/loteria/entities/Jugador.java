4fb02627877b7f19bfb34e11a4b01f3e2f9cb1a8
Jaime Ramos Lara <jaime.ramos@babelgroup.com>
2025-03-19 11:03:43 +0100

    tablas creadas

--- src/main/java/com/ejercicio/loteria/entities/Jugador.java
+++ src/main/java/com/ejercicio/loteria/entities/Jugador.java
@@ -1,15 +1,24 @@
 package com.ejercicio.loteria.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "jugador")
 public class Jugador {
+
+    @Column(name = "nombre")
     public String nombre;
+    @Id
+    private Long id;
 
     public Jugador(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Jugador() {
     }
 
     public String getNombre() {
@@ -18,5 +27,13 @@ public class Jugador {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,14 @@
 CREATE TABLE IF NOT EXISTS `apuesta` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `jugador` INT NOT NULL,
     `numero1` NUMERIC(2) NOT NULL,
     `numero2` NUMERIC(2) NOT NULL,
     `numero3` NUMERIC(2) NOT NULL,
     `numero4` NUMERIC(2) NOT NULL,
     `numero5` NUMERIC(2) NOT NULL,
-    `numero6` NUMERIC(2) NOT NULL,);
+    `numero6` NUMERIC(2) NOT NULL,
+    FOREIGN KEY (jugador) REFERENCES jugador(id));
 
 CREATE TABLE IF NOT EXISTS `jugador` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `nombre` NUMERIC(2) NOT NULL,
-    `numero2` NUMERIC(2) NOT NULL,
-    `numero3` NUMERIC(2) NOT NULL,
-    `numero4` NUMERIC(2) NOT NULL,
-    `numero5` NUMERIC(2) NOT NULL,
-    `numero6` NUMERIC(2) NOT NULL,);
+    `nombre` NUMERIC(2) NOT NULL,);

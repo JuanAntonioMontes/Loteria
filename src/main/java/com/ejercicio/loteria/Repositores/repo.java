c72b3db34db97f4fec86e9f7693b2a35d4e687b5
alberto.carmona <alberto.carmona@babelgroup.com>
2025-03-19 10:51:55 +0100

    Entidad Jugador

--- src/main/java/com/ejercicio/loteria/Repositores/repo.java
+++ src/main/java/com/ejercicio/loteria/Repositores/repo.java
@@ -1,4 +1,10 @@
 package com.ejercicio.loteria.Repositores;
 
+import com.ejercicio.loteria.entities.Jugador;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class repo {
+
+
 }
